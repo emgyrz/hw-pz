db80683908d254453e3addd35702d6419a7162d5
mz <emgyrz@gmail.com>
2022-06-25 23:15:05 +0300

    hw-01

--- pages/01/index.js
+++ pages/01/index.js
@@ -1,3 +1,22 @@
 
+const house = {
+  number: 13,
+  street: "Ленина",
+  residents: {
+    old: 5,
+    young: 3,
+    total: 21,
+  }
+}
 
-console.log('hello')
+const {     number,     residents:      people     } = house
+const {     total,      old:      elderly,      young     } = people
+
+const  countOfPeopleInDaHouse =     total     -     elderly     -     young
+
+const { x } = {     x:      countOfPeopleInDaHouse      }
+
+console.log( number - x ) // 0
+
+;
+[ 1, 2, 3 ].filter( x => x< 10 ).map( x => ([x,x])).map(c => new Point(c[0], c[1]))
