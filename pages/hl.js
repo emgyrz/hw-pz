760c449853e002a74954973d56a7b0dcee932e68
mz <emgyrz@gmail.com>
2022-07-01 00:03:31 +0300

    add styles to hw-link; add highlight.js

---
+++ pages/hl.js
@@ -0,0 +1,5 @@
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import 'highlight.js/styles/atom-one-dark.css'
+hljs.registerLanguage('javascript', javascript);
+hljs.highlightAll()
