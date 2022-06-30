f99bbe58f1f590b90f9225bc66672cea13b3bec7
mz <emgyrz@gmail.com>
2022-06-30 23:25:46 +0300

    hw 02 - array methods

--- pages/02/index.js
+++ pages/02/index.js
@@ -1,3 +1,18 @@
 
 
+class MyBestAwesomeArrayEver {
+
+  find( predicate ) { /***   - 1 -   ***/ }
+
+
+
+
+  some( predicate ) { /***   - 2 -   ***/ }
+
+
+
+
+  reduce( predicate ) { /***   - 4 -   ***/ }
+
+}
 console.log('hello')
