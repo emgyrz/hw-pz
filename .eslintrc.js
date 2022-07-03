0c12bab1abc40858ba36b0f32c0a14ea2622677c
mz <emgyrz@gmail.com>
2022-07-03 20:53:51 +0300

    update eslint cfg & format 02/.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,138 +1,148 @@
 module.exports = {
-    'root': true,
-    "parserOptions": {
-      'ecmaVersion': 12,
-      'sourceType': 'module'
+  'root': true,
+  "parserOptions": {
+    'ecmaVersion': 12,
+    'sourceType': 'module'
   },
 
-    "env": {
-      'browser': true,
-      'es2021': true,
-      'node': true
-    },
-    // 'extends': 'eslint:recommended',
-    'overrides': [
-      /**
+  "env": {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  // 'extends': 'eslint:recommended',
+  'overrides': [
+    /**
        * -----------------------------------------------------
        * TYPESCRIPT FILES (COMPONENTS, SERVICES ETC) (.ts)
        * -----------------------------------------------------
        */
-      {
-        'files': [ '*.js' ],
-        plugins: [
+    {
+      'files': [ '*.js' ],
+      plugins: [
+      ],
+
+      'rules': {
+
+        indent: [ 1, 2 ],
+        'space-unary-ops': 1,
+        semi: [ 1, 'never' ],
+        'space-infix-ops': 1,
+        'no-empty': 1,
+        'no-empty-pattern': 1,
+        'arrow-spacing': 1,
+        'arrow-parens': 1,
+        // 'no-unused-vars': 1,
+        'no-prototype-builtins': 0,
+        //
+        //
+        //
+        //
+        //
+        'array-bracket-spacing': [ 1, 'always' ],
+        'block-spacing': 1,
+        'brace-style': 1,
+        'comma-spacing': [ 2, { before: false, after: true } ],
+        'space-in-parens': [ 1, 'always' ],
+        'max-lines': 2,
+        'max-params': 2,
+        'no-tabs': 2,
+        'no-whitespace-before-property': 2,
+        'keyword-spacing': 1,
+        'space-before-blocks': 1,
+        'object-curly-spacing': [ 1, 'always' ],
+        'no-multiple-empty-lines': [ 2, { max: 7 } ],
+        'computed-property-spacing': [ 1, 'always' ],
+        'arrow-body-style': 'error',
+        'curly': 0,
+        'eol-last': 'error',
+
+
+        //
+        //
+        //
+        //
+        //
+        'no-param-reassign': [ 2, { props: false } ],
+        'complexity': 'off',
+        'constructor-super': 'error',
+        'eqeqeq': [
+          'error',
+          'smart'
         ],
+        'guard-for-in': 'error',
+        'id-blacklist': [
+          'error',
+          'any',
+          'Number',
+          'number',
+          'String',
+          'string',
+          'Boolean',
+          'boolean',
+          'Undefined',
+          'undefined'
+        ],
+        'id-match': 'error',
+        'max-classes-per-file': 'off',
+        'max-len': [
+          'error',
+          {
+            'code': 108
+          }
+        ],
+        'new-parens': 'error',
+        'no-bitwise': 'error',
+        'no-caller': 'error',
+        'no-cond-assign': 'error',
 
-        'rules': {
+        'no-eval': 'error',
+        'no-invalid-this': 'off',
+        'no-new-wrappers': 'error',
+        'no-shadow': [
+          'error',
+          {
+            'hoist': 'all'
+          }
+        ],
+        'no-throw-literal': 'error',
+        'no-trailing-spaces': 1,
+        'no-undef-init': 'error',
+        'no-underscore-dangle': 'error',
+        'no-unsafe-finally': 'error',
+        'no-unused-labels': 'error',
+        'no-var': 'error',
+        'object-shorthand': 'error',
+        'one-var': [
+          'error',
+          'never'
+        ],
+        'prefer-const': 1,
+        'radix': 'error',
+        'space-before-function-paren': [
+          'error',
+          {
+            'anonymous': 'always',
+            'asyncArrow': 'always',
+            'named': 'never'
+          }
+        ],
+        'spaced-comment': [
+          'error',
+          'always',
+          {
+            'markers': [
+              '/'
+            ]
+          }
+        ],
+        'use-isnan': 'error',
+        'valid-typeof': 'off',
 
-          //
-          //
-          //
-          //
-          //
-          'array-bracket-spacing': [ 1, 'always' ],
-          'block-spacing': 1,
-          'brace-style': 1,
-          'comma-spacing': [ 2, { before: false, after: true } ],
-          'space-in-parens': [ 1, 'always' ],
-          'max-lines': 2,
-          'max-params': 2,
-          'no-tabs': 2,
-          'no-whitespace-before-property': 2,
-          'keyword-spacing': 1,
-          'space-before-blocks': 1,
-          'object-curly-spacing': [ 1, 'always' ],
-          'no-multiple-empty-lines': [ 2, { max: 7 } ],
-          'computed-property-spacing': [ 1, 'always' ],
-          'arrow-body-style': 'error',
-          'curly': 0,
-          'eol-last': 'error',
+      }
+    },
 
-
-          //
-          //
-          //
-          //
-          //
-          'no-param-reassign': [ 2, { props: false } ],
-          'complexity': 'off',
-          'constructor-super': 'error',
-          'eqeqeq': [
-            'error',
-            'smart'
-          ],
-          'guard-for-in': 'error',
-          'id-blacklist': [
-            'error',
-            'any',
-            'Number',
-            'number',
-            'String',
-            'string',
-            'Boolean',
-            'boolean',
-            'Undefined',
-            'undefined'
-          ],
-          'id-match': 'error',
-          'max-classes-per-file': 'off',
-          'max-len': [
-            'error',
-            {
-              'code': 108
-            }
-          ],
-          'new-parens': 'error',
-          'no-bitwise': 'error',
-          'no-caller': 'error',
-          'no-cond-assign': 'error',
-
-          'no-eval': 'error',
-          'no-invalid-this': 'off',
-          'no-new-wrappers': 'error',
-          'no-shadow': [
-            'error',
-            {
-              'hoist': 'all'
-            }
-          ],
-          'no-throw-literal': 'error',
-          'no-trailing-spaces': 1,
-          'no-undef-init': 'error',
-          'no-underscore-dangle': 'error',
-          'no-unsafe-finally': 'error',
-          'no-unused-labels': 'error',
-          'no-var': 'error',
-          'object-shorthand': 'error',
-          'one-var': [
-            'error',
-            'never'
-          ],
-          'prefer-const': 'error',
-          'radix': 'error',
-          'space-before-function-paren': [
-            'error',
-            {
-              'anonymous': 'never',
-              'asyncArrow': 'always',
-              'named': 'never'
-            }
-          ],
-          'spaced-comment': [
-            'error',
-            'always',
-            {
-              'markers': [
-                '/'
-              ]
-            }
-          ],
-          'use-isnan': 'error',
-          'valid-typeof': 'off',
-
-        }
-      },
-
-      /**
+    /**
        * -----------------------------------------------------
        * COMPONENT TEMPLATES
        * -----------------------------------------------------
@@ -141,5 +151,5 @@ module.exports = {
        * object after this one to understand how they relate to this configuration directly
        * below.
        */
-    ]
-  }
+  ]
+}
--- pages/02/index.js
+++ pages/02/index.js
@@ -6,164 +6,126 @@ const cars = [ {
   name: 'BMW', price: 23100
 } ]
 
-class MyBestAwesomeArrayEver {
-  constructor(data){
-    this.data = data;
-  }
-  find( predicate ) { 
-    /***   - 1 -   ***/ 
-
-for (let i=0;this.data.length;i++){
-  let result = this.data[i];
-  if (predicate(result)==true) return result
-}
-
-}
-
-some( predicate ){ { /***   - 2 -   ***/ }
-
-for (let i=0; this.data.length; i++){
-  
-if (this[i]===true) return true;
-  
-}
-
-return;
-}
 
 
-reduce( predicate ){ { /***   - 4 -   ***/ }
+class MyBestAwesomeArrayEver extends Array {
 
-let value = arguments[0];
+  find( predicate ) {
+    /** *   - 1 -   ***/
 
-                   
-for (let i=0; i < this.data.length; i++) {
-
-  if (!(isFinite(value))) {value = predicate(0, this.data[i])} else {
-value = predicate(value, this.data[i]);}
+    for ( let i = 0;this.length;i++ ) {
+      const result = this[ i ]
+      if ( predicate( result ) === true ) return result
+    }
 
   }
 
-          return value;
+  some( predicate ) {
+    { /** *   - 2 -   ***/ }
 
+    for ( let i = 0; this.length; i++ ) {
+
+      if ( this[ i ] === true ) return true
+
+    }
+
+    return
+  }
+
+
+  reduce( predicate ) {
+    { /** *   - 4 -   ***/ }
+
+    let value = arguments[ 0 ]
+
+    for ( let i = 0; i < this.length; i++ ) {
+
+      if ( !( isFinite( value ) ) ) {
+        value = predicate( 0, this[ i ] )
+      } else {
+        value = predicate( value, this[ i ] )
+      }
+
+    }
+
+    return value
+
+  }
 }
-}
-// console.log('hello');
+
 
 // Решение 1.1
 // Почему bmw будет равно undefined? Как найти в массиве cars объект BMW?
-const bmw = cars.find( ( car ) => {
-  return car.name === 'BMW';
-} )
+const bmw = cars.find( ( car ) => car.name === 'BMW' )
 
 // Решение 1.2
 
-let car = new MyBestAwesomeArrayEver(cars);
+const car2 = MyBestAwesomeArrayEver.from( cars )
 
-console.log( car.find(( car ) => {
-  return car.name === 'BMW';
-}));
+console.log( car2.find( ( car ) => car.name === 'BMW' ) )
 // <------------------------ 2 ------------------------------->
 // Решение 2.1
 
-console.log( cars.some(( car ) => {
-  return car.price > 30000;
-}));
+console.log( cars.some( ( car ) => car.price > 30000 ) )
 
 // Решение 2.2
-console.log( cars.some(( car ) => {
-  return car.price > 50000;
-})) 
+console.log( cars.some( ( car ) => car.price > 50000 ) )
 
 // Решение 2.3
-console.log( cars.every(( car ) => {
-  return car.price > 50000;
- }));
- 
+console.log( cars.every( ( car ) => car.price > 50000 ) )
+
 // Решение 2.4
-console.log( cars.every(( car ) => {
-  return car.hasOwnProperty('name') && (car.name !==undefined) && (car.name !=='');
- }))
+console.log( cars.every(
+  ( car ) => car.hasOwnProperty( 'name' ) && ( car.name !== undefined ) && ( car.name !== '' ) )
+)
 // <------------------------ 3 ------------------------------->
- 
+
 // Решение 3.1
-console.log(cars.map(elem => elem.price).includes(27700));
+console.log( cars.map( ( elem ) => elem.price ).includes( 27700 ) )
 
 // Решение 3.2
-console.log(!!cars.find(elem=>elem.price==27700));
+console.log( !!cars.find( ( elem ) => elem.price === 27700 ) )
 
 // Решение 3.3
-console.log(cars.find(elem=>elem.name.includes('12')).name);
+console.log( cars.find( ( elem ) => elem.name.includes( '12' ) ).name )
 
 // <------------------------ 4 ------------------------------->
 // Решение 4.1
-console.log(cars.reduce((acum,elem)=>{return acum + elem.price},0));
+console.log( cars.reduce( ( acum, elem ) => acum + elem.price, 0 ) )
 
 // Решение 4.2
 
 
 // <------------------------ 5 ------------------------------->
-console.log(cars.sort((elem1, elem2)=>{return elem1.price-elem2.price}));
-
-// <------------------------ 6 ------------------------------->
-// Решение 6.1
-//
-// Вопрос как написать это решение при помощи одного console.log используя callbackи, т.е. как исправить решение ниже
-//
-// console.log((cars)=>{
-//   let answer = cars.splice(()=>{
-//  let index = cars.findIndex((elem)=>{
-
-//               if (elem.name.indexOf('в')) return true
-                                  
-//   });
-//   return index
-  
-// },1);
-
-// return cars;
-// });
+console.log( cars.sort( ( elem1, elem2 ) => elem1.price - elem2.price ) )
 
 
-cars.splice(()=>{
-  let index = cars.findIndex((elem)=>{
- 
-               if (elem.name.indexOf('в')) return true
-                                   
-   });
-   return index
-   
- },1);
- 
- console.log(cars);
+cars.splice( ( () => {
+  const index = cars.findIndex( ( elem ) => {
+
+    if ( elem.name.indexOf( 'в' ) ) return true
+
+  } )
+  return index
+
+} )(), 1 )
+
+console.log( cars )
 
 // Решение 6.2
 
-function v(obj){
-  let index = obj.findIndex((elem)=>{
- 
-               if (elem.name.indexOf('в')) return true
-                                   
-   });
-   return index
-   
- }
- 
- function transmit(obj, index){
-   obj.push(obj[index]);
-   obj.splice(index,1);
- }
- 
- // для удаления первого элемента массива с буквой в в названии
- cars.splice(()=>{
-  let index = cars.findIndex((elem)=>{
- 
-               if (elem.name.indexOf('в')) return true
-                                   
-   });
-   return index
-   
- },1);
- 
- transmit(cars, v(cars));
- console.log(cars);
+function v( obj ) {
+  return obj.findIndex( ( elem ) => elem.name.includes( 'в' ) )
+
+}
+
+function transmit( obj, index ) {
+  obj.push( obj[ index ] )
+  obj.splice( index, 1 )
+}
+
+// для удаления первого элемента массива с буквой в в названии
+cars.splice( ( () => cars.findIndex( ( elem ) => elem.name.includes( 'в' ) ) )(), 1 )
+
+transmit( cars, v( cars ) )
+console.log( cars )
